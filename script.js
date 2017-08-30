c1bd160c3daac9c031770e8413a1e197d8735617
Joshua Morales <jmorales8191@gmail.com>
2017-08-30 14:31:14 +0200

    add checkWin() and checkTie() functions to determine if game won or tied

--- script.js
+++ script.js
@@ -14,3 +14,49 @@ squares.forEach(function(square) {
     }
   });
 });
+
+
+
+function checkWin(symbol) {
+  // set winning rows
+  win = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ];
+
+  var winner = false;
+  // loop through winning rows/indices
+  for (var i = 0; i < win.length; i++) {
+    for (var j = 0; j < win[i].length; j++) {
+      // match the row/index to square, check match
+      if (squares[win[i][j]].innerText !== symbol) {
+        winner = false;
+        break;
+      }
+      winner = true;
+    }
+    // we made it to end of row, found a winner
+    if (winner) break;
+  }
+  return winner;
+}
+
+function checkTie() {
+  var tie = false;
+  for (var i = 0; i < squares.length; i++) {
+    if (squares[i].innerText === "") {
+      // not all spaces have been played yet
+      tie = false;
+      break;
+    }
+    tie = true;
+  }
+
+  return tie;
+}
